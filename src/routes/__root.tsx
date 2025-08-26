654be61c538d491111d46d52ea6332488daaceb8
shivanshu11092003 <sshivanshugupta11@gmail.com>
2025-08-26 11:20:26 +0530

    feat: createroutewithcontext

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,87 +1,94 @@
 /// <reference types="vite/client" />
 import {
   HeadContent,
-  Link,
+  Outlet,
   Scripts,
-  createRootRoute,
-} from '@tanstack/react-router'
-import * as React from 'react'
-import { DefaultCatchBoundary } from '~/components/DefaultCatchBoundary'
-import { NotFound } from '~/components/NotFound'
-import appCss from '~/styles/app.css?url'
-import { seo } from '~/utils/seo'
-import { Toaster } from 'sonner'
-import "../../setup"
+  createRootRouteWithContext,
+} from "@tanstack/react-router";
+import * as React from "react";
+import { Toaster } from "sonner";
+import { DefaultCatchBoundary } from "~/components/DefaultCatchBoundary";
+import { NotFound } from "~/components/NotFound";
+import appCss from "~/styles/app.css?url";
+import { seo } from "~/utils/seo";
+import "../../setup";
 
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
-const queryClient = new QueryClient()
-export const Route = createRootRoute({
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { MyRouterContext } from "~/utils/types";
+
+const queryClient = new QueryClient();
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
   head: () => ({
     meta: [
       {
-        charSet: 'utf-8',
+        charSet: "utf-8",
       },
       {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
       },
       ...seo({
         title:
-          'TanStack Start | Type-Safe, Client-First, Full-Stack React Framework',
+          "TanStack Start | Type-Safe, Client-First, Full-Stack React Framework",
         description: `TanStack Start is a type-safe, client-first, full-stack React framework. `,
       }),
     ],
     links: [
-      { rel: 'stylesheet', href: appCss },
+      { rel: "stylesheet", href: appCss },
       {
-        rel: 'apple-touch-icon',
-        sizes: '180x180',
-        href: '/apple-touch-icon.png',
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
       },
       {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '32x32',
-        href: '/favicon-32x32.png',
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
       },
       {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '16x16',
-        href: '/favicon-16x16.png',
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
       },
-      { rel: 'manifest', href: '/site.webmanifest', color: '#fffff' },
-      { rel: 'icon', href: '/favicon.ico' },
+      { rel: "manifest", href: "/site.webmanifest", color: "#fffff" },
+      { rel: "icon", href: "/favicon.ico" },
     ],
     scripts: [
       {
-        src: '/customScript.js',
-        type: 'text/javascript',
+        src: "/customScript.js",
+        type: "text/javascript",
       },
     ],
   }),
+  component: RootComponent,
   errorComponent: DefaultCatchBoundary,
   notFoundComponent: () => <NotFound />,
-  shellComponent: RootDocument,
-})
+});
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
+    </RootDocument>
+  );
+}
 
 function RootDocument({ children }: { children: React.ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <html className='dark'>
-        <head>
-          <HeadContent />
-        </head>
-        <body>
-          {children}
-          <Scripts />
-          <Toaster position="top-center" richColors />
-
-        </body>
-      </html>
-    </QueryClientProvider>
-  )
+    <html className="dark">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+        <Toaster position="top-center" richColors />
+      </body>
+    </html>
+  );
 }
---
+++ src/utils/types.ts
@@ -0,0 +1,5 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export type MyRouterContext = {
+  queryClient: QueryClient;
+};
